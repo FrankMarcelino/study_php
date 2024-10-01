e8eaa1491ae19062d69e95c146cf627de7f4a151
frankMarcelino <frank.silva.ti@gmail.com>
2024-10-01 10:47:58 -0300

    hello word

---
+++ CV_PHP/ex000/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>
+    <?php
+        echo "Hello World! \u{1F30E}";
+    ?>
+  </h1>
+  <p>
+    meu primeiro hellor word em php
+  </p>
+</body>
+
+</html>
